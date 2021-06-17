6c3ed6ff09ebcf3c2dbee8c494dcd8a05876f939
AnthonyOpenSource <44232840+AnthonyOpenSource@users.noreply.github.com>
2021-06-17 18:54:05 +0200

    Update api.bat

--- api.bat
+++ api.bat
@@ -1,12 +1,4 @@
 @echo off
-del GreentegCoreApp1.csproj
-del GreentegCoreApp1.sln
-xcopy base\*.* . /r
-color 47
-echo /!\ For PcLink only use the default PcLinkServerBase in Util.cs for testing, do not use it for commercial or even regular personal use. The data dosen't get deleted and i have not a lot space on the server. /!\
-pause
-color 07
-
 echo Certificate Configuration
 echo Distributor  Password:%distributorpass%
 echo              Path:    %distributorcert%
@@ -15,7 +7,7 @@ echo              Path:    %developercert%
 
 echo Assembling new csproj
 
-SetCsprojPass %developercert% %developerpass% %distributorcert% %distributorpass% GreentegCoreApp1.csproj GreentegCoreApp1.csproj
+SetCsprojPass %developercert% %developerpass% %distributorcert% %distributorpass% AppSourceCode\GreentegCoreApp1.csproj AppSourceCode\GreentegCoreApp1.csproj
 
 del cstzpass.csproj
 pause
